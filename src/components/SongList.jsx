8cfe8343dba07ee5c85046fbbc3267dd31aaa159
chhaviGupta986 <94625954+chhaviGupta986@users.noreply.github.com>
2024-07-18 02:30:13 +0530

    added loader

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -4,17 +4,6 @@ import './SongList.css'
 
 const SongCard = ({ song }) => {
   return (
-    // <div className="card d-flex flex-row">
-    //   <img src={song.imgUrl} alt="Song" style={{ width: '100px', height: '100px' }}/>
-    //   <div className="container">
-    //     <h4><b>{song.title}</b></h4>
-    //     <p>{song.artist}</p>
-    //     <audio controls>
-    //       <source src={song.songUrl} type="audio/mpeg" />
-    //       Your browser does not support the audio element.
-    //     </audio>
-    //   </div>
-    // </div>
    <div className='container'  >
    
 <div id='idmain' style={{ alignContent:'center',justifyContent:'space-evenly',margin:'15px',
@@ -32,8 +21,6 @@ paddingRight:'5px'
      }}>
       {/* <img src={song.imgUrl} alt="Song" style={{ width: '270px', height: '250px' ,objectFit: 'cover'}}/> */}
       <div className="container" style={{flex:'1 0 21%',margin:'5%',height:'100px'}}>
-        {/* <h4><b>{song.title}</b></h4>
-        <p>{song.artist}</p> */}
         <audio controls >
           <source src={song.songsUrl} type="audio/mpeg" />
           Your browser does not support the audio element.
@@ -50,27 +37,41 @@ paddingRight:'5px'
 
 const SongList = () => {
   const [songs, setSongs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('https://tuning-backend.onrender.com/api/songs');
         setSongs(response.data);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching songs:', error);
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
+  
+  if (loading) {
+    return <>
+
+    <div className='text-white loader'>Loading</div>
+
+    </>;
+  }
+  
 
   return (
     <div className='d-flex flex-row row-3' style={{ display: 'flex', flexWrap: 'wrap' }}>
+    
   {songs.map((song, index) => (
     <div key={index} className="song-list d-flex flex-col" style={{ width: '24%', margin: '5px' }}>
       <SongCard song={song} />
     </div>
   ))}
+  
 </div>
 
 
--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,20 +2,7 @@
 import { getApp,getApps,initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from 'firebase/storage';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBUy6X_Cl5ErNIt5NXIRPEgdxya1ff9w98",
-//   authDomain: "tuning-4a5ee.firebaseapp.com",
-//   projectId: "tuning-4a5ee",
-//   storageBucket: "tuning-4a5ee.appspot.com",
-//   messagingSenderId: "1069455325595",
-//   appId: "1:1069455325595:web:d723ca2d86ac6e6e0c893e",
-//   measurementId: "G-L64G0JTTTB"
-// };
 const firebaseConfig = {
   apiKey: "AIzaSyCsvVSECi6Mb7wdSZpkDC9zEFohjLafpOU",
   authDomain: "tuning-9ab11.firebaseapp.com",
